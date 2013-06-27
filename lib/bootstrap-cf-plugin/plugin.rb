6ff4e1f06952d744e8720bf08621ea4dc595672d
Ryan Tang <pair+ryantang@pivotallabs.com>
2013-06-27 15:14:16 -0700

    Refactor bootstrap's plugin.rb

--- lib/bootstrap-cf-plugin/plugin.rb
+++ lib/bootstrap-cf-plugin/plugin.rb
@@ -6,13 +6,72 @@ module BootstrapCfPlugin
       # skip all default preconditions
     end
 
-    def lookup_infrastructure_class(infrastructure)
-      infrastructure_class = infrastructure.to_s.capitalize
-      infrastructure_module = ::BootstrapCfPlugin::Infrastructure
-      if infrastructure_module.const_defined?(infrastructure_class)
-        infrastructure_module.const_get(infrastructure_class)
-      else
-        raise "Unsupported infrastructure #{infrastructure}"
+
+    desc "Bootstrap a CF deployment"
+    group :admin
+    input :infrastructure, :argument => :required, :desc => "The infrastructure to bootstrap and deploy"
+    input :template, :argument => :optional, :desc => "The template file for the CF deployment"
+    def bootstrap
+      DirectorCheck.check
+      infrastructure_class.bootstrap(input[:template])
+
+      invoke :logout
+      invoke :target, :url => cloud_controller_url
+
+      login_as_uaa_user
+      org = find_or_create_org("bootstrap-org")
+      space = find_or_create_space(org, "bootstrap-space")
+      login_as_uaa_user
+      invoke :target, :url => cloud_controller_url, :organization => org, :space => space
+
+      insert_services_tokens
+
+      puts "All done with bootstrap!"
+    end
+
+    desc "Generate a manifest stub"
+    group :admin
+    input :infrastructure, :argument => :required, :desc => "The infrastructure for which to generate a stub"
+    def generate_stub
+      DirectorCheck.check
+      SharedSecretsFile.find_or_create("cf-shared-secrets.yml")
+      infrastructure_class.generate_stub("cf-#{infrastructure}-stub.yml", "cf-shared-secrets.yml")
+    end
+
+    private
+
+    def infrastructure
+      input[:infrastructure] || raise("Infrastructure must be specified")
+    end
+
+    def cf_manifest
+      @cf_manifest ||= load_yaml_file("cf-#{infrastructure}.yml")
+    end
+
+    def cf_services_manifest
+      load_yaml_file("cf-services-#{infrastructure}.yml")
+    end
+
+    def uaa_user
+      cf_manifest.fetch('properties').fetch('uaa').fetch('scim').fetch('users').first.split("|")
+    end
+
+    def uaa_user_login
+      uaa_user[0]
+    end
+
+    def uaa_user_pw
+      uaa_user[1]
+    end
+
+    def cloud_controller_url
+      cf_manifest.fetch('properties').fetch('cc').fetch('srv_api_uri')
+    end
+
+    def login_as_uaa_user
+      begin
+        invoke :login, :username => uaa_user[0], :password => uaa_user[1]
+      rescue CF::UserFriendlyError, /There are no (organizations|spaces)/
       end
     end
 
@@ -28,37 +87,7 @@ module BootstrapCfPlugin
       end
     end
 
-    desc "Bootstrap a CF deployment"
-    group :admin
-    input :infrastructure, :argument => :required, :desc => "The infrastructure to bootstrap and deploy"
-    input :template, :argument => :optional, :desc => "The template file for the CF deployment"
-    def bootstrap
-      infrastructure = input[:infrastructure]
-      infrastructure_class = lookup_infrastructure_class(infrastructure)
-      DirectorCheck.check
-      infrastructure_class.bootstrap(input[:template])
-
-      cf_manifest = load_yaml_file("cf-#{infrastructure}.yml")
-      cf_services_manifest = load_yaml_file("cf-services-#{infrastructure}.yml")
-      cf_properties = cf_manifest.fetch('properties')
-      uaa_users = cf_properties.fetch('uaa').fetch('scim').fetch('users')
-
-      uaa_user = uaa_users.first.split("|")
-
-      invoke :logout
-      invoke :target, :url => cf_properties.fetch('cc').fetch('srv_api_uri')
-
-      begin
-        invoke :login, :username => uaa_user[0], :password => uaa_user[1]
-      rescue CF::UserFriendlyError, /There are no (organizations|spaces)/
-      end
-
-      org = find_or_create_org("bootstrap-org")
-
-      space = find_or_create_space(org, "bootstrap-space")
-      invoke :target, :url => cf_properties.fetch('cc').fetch('srv_api_uri'), :organization => org, :space => space
-
-      # invoke a bunch of create-service-token commands
+    def insert_services_tokens
       (tokens_from_jobs(cf_services_manifest.fetch('jobs', []))).each do |gateway_info|
         begin
           invoke :create_service_auth_token, gateway_info
@@ -66,29 +95,15 @@ module BootstrapCfPlugin
           puts "  Don't worry, service token already installed, continuing"
         end
       end
-      puts "All done with bootstrap!"
     end
-
-
-    desc "Generate a manifest stub"
-    group :admin
-    input :infrastructure, :argument => :required, :desc => "The infrastructure for which to generate a stub"
-    def generate_stub
-      infrastructure = input[:infrastructure]
-      infrastructure_class = lookup_infrastructure_class(infrastructure)
-      DirectorCheck.check
-      SharedSecretsFile.find_or_create("cf-shared-secrets.yml")
-      infrastructure_class.generate_stub("cf-#{infrastructure}-stub.yml", "cf-shared-secrets.yml")
-    end
-
-    private
 
     def find_or_create_org(name)
       org = client.organization_by_name(name)
 
       unless org
-        invoke :create_org, :name => name
+        invoke :create_org, :name => name, :target => false
         org = client.organization_by_name(name)
+        puts org.inspect
       end
       org
     end
@@ -98,9 +113,20 @@ module BootstrapCfPlugin
 
       unless space
         invoke :create_space, :organization => org, :name => name
-        space = client.space_by_name(org)
+        space = client.space_by_name(name)
       end
       space
     end
   end
 end
+
+def infrastructure_class
+  infrastructure_class = infrastructure.to_s.capitalize
+  infrastructure_module = ::BootstrapCfPlugin::Infrastructure
+  if infrastructure_module.const_defined?(infrastructure_class)
+    infrastructure_module.const_get(infrastructure_class)
+  else
+    raise "Unsupported infrastructure #{infrastructure}"
+  end
+end
+
